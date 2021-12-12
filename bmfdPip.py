c9f77e2ad2e0cc4ef80a23413876412c98ce7ec7
SushiMakiPhillipeTeo <90980259+SushiMakiPhillipeTeo@users.noreply.github.com>
2021-12-12 11:15:10 +0100

    Delete bmfdPip.py

--- bmfdPip.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import pip
-
-def install(package):                                   # Install require file for BMFD
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
